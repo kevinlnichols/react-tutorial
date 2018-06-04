92c8b8e4966792723131ec1b9525122055025db2
Kevin Nichols <kevinlnichols@gmail.com>
2018-06-04 16:02:07 -0400

    first commit

---
+++ src/components/todoItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+
+  render() {
+    return (
+      <li className="todo">
+        <strong>{this.props.todo.title}</strong>
+      </li>
+    );
+  }
+}
+
+export default TodoItem;
